ba0e5e9d1b85c68fa582a041e66333b7b3a8086f
Suttiluk <suttiluk@thinknet.co.th>
2018-05-04 14:22:53 +0700

    conflict \

--- front-app/src/components/Content.js
+++ front-app/src/components/Content.js
@@ -1,23 +1,10 @@
-import React, { Component } from 'react'
-import { Layout, Menu, Breadcrumb, Icon } from 'antd'
-import firebase from 'firebase'
-
+import React, { Component } from 'react';
+import { Layout, Menu, Icon } from 'antd';
 const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
-
-const config = {
-  apiKey: "AIzaSyAPHQAAG6znTUbG6PL7Oiw2hwI50lrPtQY",
-  authDomain: "daily-report-14ce7.firebaseapp.com",
-  databaseURL: "https://daily-report-14ce7.firebaseio.com",
-  projectId: "daily-report-14ce7",
-  storageBucket: "daily-report-14ce7.appspot.com",
-  messagingSenderId: "545247969478"
-};
-firebase.initializeApp(config);
+const {  Content, Sider } = Layout;
 
 class Contents extends Component {
   render() {
-    console.log(firebase.app().name)
     return (
         <Layout>
         <Content className="contentAll">
@@ -29,27 +16,22 @@ class Contents extends Component {
                 defaultOpenKeys={['sub1']}
                 style={{ height: '100%' }}
               >
-              <SubMenu key="sub" title={<span><Icon type="apple" />Team </span>}>
                 <SubMenu key="sub1" title={<span><Icon type="apple" />Team P'Set</span>}>
-                  <Menu.Item key="1">name</Menu.Item>
-                  <Menu.Item key="2">name</Menu.Item>
-                  <Menu.Item key="3">name</Menu.Item>
-                  <Menu.Item key="4">name</Menu.Item>
+                  <Menu.Item key="1">option1</Menu.Item>
+                  <Menu.Item key="2">option2</Menu.Item>
+                  <Menu.Item key="3">option3</Menu.Item>
+                  <Menu.Item key="4">option4</Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub2" title={<span><Icon type="apple-o" />Team P'Ice</span>}>
-                  <Menu.Item key="5">name</Menu.Item>
-                  <Menu.Item key="6">name</Menu.Item>
-                  <Menu.Item key="7">name</Menu.Item>
-                  <Menu.Item key="8">name</Menu.Item>
-                </SubMenu>
+                  <Menu.Item key="5">option5</Menu.Item>
+                  <Menu.Item key="6">option6</Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
                 </SubMenu>
               </Menu>
             </Sider>
             <Content className="contentRight" >
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
+              Content
             </Content>
           </Layout>
         </Content>
