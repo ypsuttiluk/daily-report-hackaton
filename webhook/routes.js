40a04d25fb9b9c8b133e6a859533b0ec5ad4df67
patcharapon wangtiyong <patcharapon@thinknet.co.th>
2018-05-04 16:53:23 +0700

    init webhook

---
+++ webhook/routes.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const lineController = require('./controller/line')
+const router = express.Router()
+
+router.post('/webhook', (req, res) => {
+  let reply_token = req.body.events[0].replyToken
+  const message = req.body.events[0].message.text
+  const type = message.split(" ")[0]
+  const name = typeof message.split(" ")[1] === 'undefined' ? null : message.split(" ")[1]
+  // if(name === null) {
+  //   return res.sendStatus(200)
+  // }
+  if(type === 'register') {
+    lineController.register(reply_token, req.body.events[0].source.userId, name)
+      return res.sendStatus(200)
+  }
+  lineController.reply(reply_token, req.body.events[0].message, req.body.events[0].source.userId)
+  return res.sendStatus(200)
+})
+router.get('/daily/time', (req, res) => {
+  lineController.dailyTime()
+  return res.sendStatus(200)
+})
+router.post('/push/message', (req, res) => {
+  lineController.pushMessage()
+  res.sendStatus(200)
+})
+// router.post('/database', (req, res) => {
+//   const body = req.body
+//   const rootRef = firebaseApp.database().ref('users')
+//   const newMessageRef = rootRef.push()
+//   newMessageRef.set({
+//   'user_id': 'ada',
+//   'text': 'The Analytical Engine weaves algebraical patterns just as the Jacquard loom weaves flowers and leaves.'
+//   })
+//   const path = newMessageRef.toString()
+//   console.log('path : ', path)
+//   console.log('body : ', body)
+//   res.sendStatus(200)
+// })
+
+module.exports = router
