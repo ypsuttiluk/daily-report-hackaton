b68a52566778205c911476091d5b7481d42e93b8
Suttiluk <suttiluk@thinknet.co.th>
2018-05-04 14:27:44 +0700

    Connect firebase

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import firebase from 'firebase'
 
-import Header from './Header'
-import Contents from './Contents'
+import Header from './components/Header'
+import Contents from './components/Content'
 import './App.css';
 
+const config = {
+  apiKey: "AIzaSyAPHQAAG6znTUbG6PL7Oiw2hwI50lrPtQY",
+  authDomain: "daily-report-14ce7.firebaseapp.com",
+  databaseURL: "https://daily-report-14ce7.firebaseio.com",
+  projectId: "daily-report-14ce7",
+  storageBucket: "daily-report-14ce7.appspot.com",
+  messagingSenderId: "545247969478"
+}
+firebase.initializeApp(config)
 
 class App extends Component {
   render() {
+    console.log(firebase.app().name)
     return (
       <div >
         <Header />
